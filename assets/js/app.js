50b3a92723fc64228c849985c861cab6f3e2c379
Adk_alfin <alfinalfarisi7@gmail.com>
2024-03-04 08:07:16 +0700

    feat(add): initial template Bootstrap@5.2.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,20 @@
 const payments = document.getElementsByName("paymentMethod");
-const pinc = document.getElementById("pinc");
 const creditCard = document.getElementById("creditCard");
 const debitCard = document.getElementById("debitCard");
 const paypalCard = document.getElementById("paypalCard");
 
 payments.forEach((item) => {
   item.addEventListener("change", (event) => {
-    if (event.target.value == "credit") {
-      pinc.textContent = "Credit Card";
+    console.log(event.target.value);
+    if (event.target.value == "creditCard") {
       creditCard.classList.remove("d-none");
       debitCard.classList.add("d-none");
       paypalCard.classList.add("d-none");
-    } else if (event.target.value == "debit") {
-      pinc.textContent = "Bank Transfer";
+    } else if (event.target.value == "debitCard") {
       debitCard.classList.remove("d-none");
       creditCard.classList.add("d-none");
       paypalCard.classList.add("d-none");
     } else {
-      pinc.textContent = "PayPal";
       paypalCard.classList.remove("d-none");
       debitCard.classList.add("d-none");
       creditCard.classList.add("d-none");
